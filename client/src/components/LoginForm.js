9ade6a6b60898a1a0d5ba549c829447275865a2f
mffonua <108596346+mffonua@users.noreply.github.com>
2023-01-05 22:09:23 -0700

    finished add and remove book

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,22 +1,15 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
 
-// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
-
-import { useMutation } from '@apollo/react-hooks';
-import { LOGIN_USER } from '../utils/mutations';
-
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
-  //code brought in after
-  const[loginUser, {error}] = useMutation(LOGIN_USER);
-
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,37 +20,22 @@ const LoginForm = () => {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
-     // const form = event.currentTarget;
-    //if (form.checkValidity() === false) {
-     // event.preventDefault();
-     // event.stopPropagation();
-   // }
 
-   // try {
-    //  const response = await loginUser(userFormData);
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-    //  if (!response.ok) {
-   //     throw new Error('something went wrong!');
-    //  }
+    try {
+      const { data } = await login({ variables: {...userFormData}})
 
-   //   const { token, user } = await response.json();
-   //   console.log(user);
-   //   Auth.login(token);
-  //  } catch (err) {
-  //    console.error(err);
-  //    setShowAlert(true);
-  //  }
-  try {
-    const { data } = await loginUser({
-      variables: { ...userFormData}
-    });
-
-    Auth.login(data.login.token)
-
-    // console.log(data);
-  } catch (e) {
-    console.error(e);
-  }
+      console.log(data);
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
 
     setUserFormData({
       username: '',
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,6 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
-
 const resolvers = {
     Query: {
       me: async(parent, args, context) => {
@@ -14,24 +13,46 @@ const resolvers = {
       }
     },
     Mutation: {
-      addUser: async(parent, args) => {
-          const user = await User.create(args)
-          const token = signToken(user)
-          return { user, token }
-      },
-      login: async(parent, { email, password }) => {
-          const user = await User.findOne({ email })
-          if(!user) {
-              throw new AuthenticationError('No existing user')
-          }
-          const correctPw = await user.isCorrectPassword(password)
-          if (!correctPw) {
-              throw new AuthenticationError('Wrong password')
-          }
-      },
-     // saveBook: async
-     // removeBook: async 
-    }
-  };
+        addUser: async(parent, args) => {
+            const user = await User.create(args)
+            const token = signToken(user)
+            return { user, token }
+        },
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email })
+            if(!user) {
+                throw new AuthenticationError('No existing user')
+            }
+            const correctPw = await user.isCorrectPassword(password)
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong password')
+            }
+            const token = signToken(user)
+            return { token, user }
+        },
+       saveBook: async(parent, { bookData }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+            {_id: context.user._id},
+            {$push: {savedBooks: bookData}},
+            {new: true}
+            )
+            return updatedUser;
+        }
+        throw new AuthenticationError('Cant save book')
+       },
+         removeBook: async(parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks: {bookId}}},
+                    {new: true}
+                )
+                return updatedUser;
+            }
+            throw new AuthenticationError('cant remove book')
+         }
+      }
+    };
 
 module.exports = resolvers;
